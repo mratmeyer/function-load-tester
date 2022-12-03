8215b1dc8f1abfe575292c5e07c3842f13f61c3a
Max Ratmeyer <17440617+mratmeyer@users.noreply.github.com>
2022-12-03 17:23:55 -0500

    Add functionsCount and dataPath flags

--- function-load-tester.go
+++ function-load-tester.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -18,15 +19,25 @@ func ping(n int) {
 }
 
 func main() {
-	var filePath = "invocations_per_function_md.anon.d01.csv"
+	// Parse CSV file, -dataPath flag
+	var dataPath string
+	flag.StringVar(&dataPath, "dataPath", "invocations_per_function_md.anon.d01.csv", "dataPath")
 
-	file, err := os.Open(filePath)
+	// Parse functionsCount, -functionsCount flag
+	var functionsCount int
+	flag.IntVar(&functionsCount, "functionsCount", 1, "functionsCount")
+
+	flag.Parse()
+
+	file, err := os.Open(dataPath)
 	if err != nil {
-		log.Fatal("Unable to read input file "+filePath, err)
+		log.Fatal("Unable to read file "+dataPath, err)
 	}
 	defer file.Close()
 
 	var data [100][]string
+	// var data []string
+	// data = make([]string, *functionsCount)
 	var index int = 0
 
 	csvReader := csv.NewReader(file)
@@ -37,17 +48,17 @@ func main() {
 		// Read row
 		record, err := csvReader.Read()
 
+		// Stop when reached end of file
+		if err == io.EOF || index == functionsCount-1 {
+			break
+		}
+
 		data[index] = record
 
 		index++
-
-		// Stop when reached end of file
-		if err == io.EOF || index == 99 {
-			break
-		}
 	}
 	if err != nil {
-		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+		log.Fatal("Unable to parse file as CSV for "+dataPath, err)
 	}
 
 	// fmt.Println(data)
@@ -55,7 +66,7 @@ func main() {
 	var column int = 4
 
 	for {
-		for i := 1; i < len(data)-1; i++ {
+		for i := 1; i < functionsCount-1; i++ {
 			// fmt.Println(data[0][column])
 			// fmt.Println(i, column)
 			var hits = data[i][column]
